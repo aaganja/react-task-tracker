e3ea546ba22a1db67748ea63a31da1cd82dd5224
Kuber aaganja <kuber@gurzu.com>
2021-08-13 12:04:03 +0545

    setup the json-server

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
@@ -6,23 +6,25 @@ import AddTask from './components/AddTask'
 const App = () => {
   const [ showAddTask, setShowAddTask ] = useState(false)
   const [ tasks, setTasks ] = useState(
-    [{
-    id: 1,
-    text: "Doctors Appointment",
-    day: 'Feb 9th at 2:30pm',
-    reminder: true
-  },{
-    id: 2,
-    text: "Meeting at Achool",
-    day: 'Feb 10th at 1:30pm',
-    reminder: true
-  },{
-    id: 3,
-    text: "Food Shopping",
-    day: 'Feb 11th at 5:30pm',
-    reminder: false
-  }]
+    []
   )
+
+  useEffect(() => {
+    const getTasks = async () => {
+      const tasksFromServer = await fetchTasks()
+      setTasks(tasksFromServer)
+    }
+
+    getTasks()
+  }, [])
+
+  // Fetch Tasks
+  const fetchTasks = async () => {
+    const res = await fetch('http://localhost:5000/tasks')
+    const data = await res.json()
+
+    return data
+  }
 
   // Add Task
   const addTask = (task) => {
